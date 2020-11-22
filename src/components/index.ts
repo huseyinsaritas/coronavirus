e96fbd27305872e5f1a55ed55c9c867000e17688
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2020-11-22 17:34:47 +0300

    index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import SearchBar from "./search-bar.component";
+import CountryPieChart from "./country-pie-chart";
+
+export { Text } from "./text.component";
+export { Button } from "./button.component";
+export { Icon } from "./icon.component";
+export { BottomTabs } from "./bottom-tabs.component";
+export { SearchBar, CountryPieChart };
